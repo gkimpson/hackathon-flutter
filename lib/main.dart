aa8bb3102f505348c6d2d038ba3cd3a1db7e0a9a
Gavin Kimpson <gkimpson@gmail.com>
2021-06-15 14:39:22 +0100

    quick push

---
+++ lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:local_auth/local_auth.dart';
+
+void main() => runApp(MyApp());
+class MyApp extends StatelessWidget {
+// This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Hackathon App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+class _MyHomePageState extends State<MyHomePage> {
+  final LocalAuthentication _localAuthentication = LocalAuthentication();
+  String _message = "Not Authorized";
+
+  Future<bool> checkingForBioMetrics() async {
+    bool canCheckBiometrics = await _localAuthentication.canCheckBiometrics;
+    print(canCheckBiometrics);
+    return canCheckBiometrics;
+  }
+
+  Future<void> _authenticateMe() async {
+// 8. this method opens a dialog for fingerprint authentication.
+//    we do not need to create a dialog nut it popsup from device natively.
+    bool authenticated = false;
+    try {
+      authenticated = await _localAuthentication.authenticateWithBiometrics(
+        localizedReason: "Authenticate for Testing", // message for dialog
+        useErrorDialogs: true, // show error in dialog
+        stickyAuth: true, // native process
+      );
+      setState(() {
+        _message = authenticated ? "Authorized" : "Not Authorized";
+      });
+    } catch (e) {
+      print(e);
+    }
+    if (!mounted) return;
+  }
+
+  @override
+  void initState() {
+// TODO: implement initState
+    checkingForBioMetrics();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Text("$_message"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _authenticateMe,
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
